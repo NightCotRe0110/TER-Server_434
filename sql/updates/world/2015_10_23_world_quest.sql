7727d038558eb89623205fd46b281d62fdc5f48a
cooler-SAI <cooler19860212@yahoo.com>
2015-10-23 21:28:30 +0300

    DB/Quest: fix from LKArthas , closed https://github.com/cooler-SAI/CataCore/issues/8

--- sql/updates/world/2015_10_23_world_quest.sql
+++ sql/updates/world/2015_10_23_world_quest.sql
@@ -11,3 +11,14 @@ INSERT INTO smart_scripts (entryorguid, source_type, id, link, event_type, event
 INSERT INTO smart_scripts (entryorguid, source_type, id, link, event_type, event_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, action_type, action_param1, action_param2, action_param3, action_param4, action_param5, action_param6, target_type, target_param1, target_param2, target_param3, target_x, target_y, target_z, target_o, comment) VALUES (42689, 0, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 56, 58120, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Skittering Spiderling on Gossip Hello - Add Item');
 INSERT INTO smart_scripts (entryorguid, source_type, id, link, event_type, event_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, action_type, action_param1, action_param2, action_param3, action_param4, action_param5, action_param6, target_type, target_param1, target_param2, target_param3, target_x, target_y, target_z, target_o, comment) VALUES (42689, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 41, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Skittering Spiderling on Gossip Hello - Despawn');
 INSERT INTO smart_scripts (entryorguid, source_type, id, link, event_type, event_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, action_type, action_param1, action_param2, action_param3, action_param4, action_param5, action_param6, target_type, target_param1, target_param2, target_param3, target_x, target_y, target_z, target_o, comment) VALUES (42689, 0, 3, 0, 64, 0, 100, 0, 0, 0, 0, 0, 74, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Skittering Spiderling on Gossip Hello - Close Gossip Whitout Quest');
+
+
+--- Dalaran Cooking Dailys Currency Fix ----
+UPDATE quest_template SET RewardCurrencyId1=81 WHERE Id=13100 LIMIT 1;
+UPDATE quest_template SET RewardCurrencyId1=81 WHERE Id=13101 LIMIT 1;
+UPDATE quest_template SET RewardCurrencyId1=81 WHERE Id=13102 LIMIT 1;
+UPDATE quest_template SET RewardCurrencyId1=81 WHERE Id=13103 LIMIT 1;
+UPDATE quest_template SET RewardCurrencyId1=81 WHERE Id=13112 LIMIT 1;
+UPDATE quest_template SET RewardCurrencyId1=81 WHERE Id=13113 LIMIT 1;
+UPDATE quest_template SET RewardCurrencyId1=81 WHERE Id=13114 LIMIT 1;
+UPDATE quest_template SET RewardCurrencyId1=81 WHERE Id=13115 LIMIT 1;
