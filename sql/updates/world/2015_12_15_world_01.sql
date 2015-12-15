1ca720965229b8e1daa3c2f65ccf9a29cbc68bb1
cooler-SAI <cooler19860212@yahoo.com>
2015-12-15 18:52:56 +0300

    Обновление скриптов спеллов,боссов,мобов

---
+++ sql/updates/world/2015_12_15_world_01.sql
@@ -0,0 +1,42 @@
+
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(91303, 'spell_chogall_conversion'),
+(93203, 'spell_chogall_conversion'),
+(93204, 'spell_chogall_conversion'),
+(93205, 'spell_chogall_conversion'),
+(81701, 'spell_chogall_corrupted_blood'),
+(93187, 'spell_chogall_corrupted_blood'),
+(93188, 'spell_chogall_corrupted_blood'),
+(93189, 'spell_chogall_corrupted_blood'),
+(81713, 'spell_chogall_corruption'),
+(93175, 'spell_chogall_corruption'),
+(93176, 'spell_chogall_corruption'),
+(93177, 'spell_chogall_corruption'),
+(81689, 'spell_chogall_corruption'),
+(93184, 'spell_chogall_corruption'),
+(93185, 'spell_chogall_corruption'),
+(93186, 'spell_chogall_corruption'),
+(81761, 'spell_chogall_corruption'),
+(93172, 'spell_chogall_corruption'),
+(93173, 'spell_chogall_corruption'),
+(93174, 'spell_chogall_corruption'),
+(82919, 'spell_chogall_corruption'),
+(93108, 'spell_chogall_corruption'),
+(93109, 'spell_chogall_corruption'),
+(93110, 'spell_chogall_corruption'),
+(82363, 'spell_chogall_corruption'),
+(93169, 'spell_chogall_corruption'),
+(93170, 'spell_chogall_corruption'),
+(93171, 'spell_chogall_corruption'),
+(93103, 'spell_chogall_corruption_bar'),
+(81628, 'spell_chogall_summon_corrupting_adherent'),
+(92393, 'spell_rupture_periodic'),
+(92300, 'spell_crystal_storm'),
+(81638, 'spell_crystal_barrage'),
+(75846, 'spell_karsh_quicksilver_armor'),
+(75854, 'spell_karsh_quicksilver_armor'),
+(93567, 'spell_karsh_quicksilver_armor');
+REPLACE INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (725, 0, 'instance_the_stonecore', 0);
+UPDATE `creature_template` SET `ScriptName` = 'mob_millhouse_manastorm' WHERE `entry` = 43391;
+UPDATE `creature_template` SET `ScriptName` = 'stonecore_teleport' WHERE `entry` = 51396;
+UPDATE `creature_template` SET `npcflag` = 1 WHERE `entry` = 51396;
