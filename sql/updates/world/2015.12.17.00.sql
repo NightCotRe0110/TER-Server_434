ea60d669133fc0a17857ec6c4574e01b9a49ff7b
Александр <ter884.markin@yandex.ru>
2015-12-17 21:44:27 +0300

    Стартовая зона Отрекшийся

---
+++ sql/updates/world/2015.12.17.00.sql
@@ -0,0 +1,17 @@
+﻿-- Тирисфальские Леса
+UPDATE `creature_template` SET `ScriptName`= "npc_aradne_50372" WHERE `entry`=50372;
+UPDATE `creature_template` SET `ScriptName`= "npc_deathknell_grave_target" WHERE `entry`=50373;
+UPDATE `creature_template` SET `ScriptName`= "npc_risen_dead" WHERE `entry`=50374;
+UPDATE `creature_template` SET `ScriptName`= "npc_undertaker_mordo" WHERE `entry`=1568;
+UPDATE `creature_template` SET `ScriptName`= "npc_darnell_49141", `AIName`= "" WHERE `entry`=49141;
+UPDATE `creature_template` SET `ScriptName`= "npc_marshal_redpath_49230" WHERE `entry`=49230;
+UPDATE `creature_template` SET `ScriptName`= "npc_valdred_moray_49231" WHERE `entry`=49231;
+UPDATE `creature_template` SET `ScriptName`= "npc_lilian_voss_38895" WHERE `entry`=38895;
+UPDATE `creature_template` SET `ScriptName`= "npc_deathguard_saltain" WHERE `entry`=1740;
+UPDATE `creature_template` SET `ScriptName`= "npc_scarlet_corpse_49340" WHERE `entry`=49340;
+UPDATE `creature_template` SET `ScriptName`= "npc_darnell_49337", `AIName`= "" WHERE `entry`=49337;
+UPDATE `creature_template` SET `ScriptName`= "npc_lilian_voss_38910" WHERE `entry`=38910;
+UPDATE `creature_template` SET `ScriptName`= "npc_rotbrain_berserker" WHERE `entry`=49422;
+UPDATE `creature_template` SET `ScriptName`= "npc_rotbrain_magus" WHERE `entry`=49423;
+UPDATE `creature_template` SET `ScriptName`= "npc_marshal_redpath" WHERE `entry`=49424;
+UPDATE `creature_template` SET `ScriptName`= "npc_deathguard_protector" WHERE `entry`=49428;
---
+++ sql/updates/world/2015.12.17.01.sql
@@ -0,0 +1,1 @@
+insert  into `npc_spellclick_spells`(`npc_entry`,`spell_id`,`cast_flags`,`user_type`) values (49340,85584,1,0);
